f10f13c184e0f9c27079a4e66369437022e90799
Suvansh Rana <36293610+suvnshr@users.noreply.github.com>
2023-12-16 22:23:44 +0530

    feat(favicon): Added rel icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,8 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
 
+        <link rel="icon" href="/magic-wand.svg" />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
